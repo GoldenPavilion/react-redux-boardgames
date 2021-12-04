ef347f6ab4686036990bdfebc93cf90e3075d1a4
GoldenPavilion <rcorystine@gmail.com>
2021-12-04 16:04:59 -0500

    created game_params in games controller

--- bg-backend/app/controllers/api/games_controller.rb
+++ bg-backend/app/controllers/api/games_controller.rb
@@ -8,4 +8,11 @@ class Api::GamesController < ApplicationController
         @game = Game.find(params[:id])
         render json: @game
     end
+
+    private
+
+    def game_params
+        params.require(:user).permit(:name, :year_published, :min_players, :max_players, :min_playtime, :max_playtime, :description_preview)
+    end
+
 end
