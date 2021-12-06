6c54576254d4961522c061b179437a6681188076
GoldenPavilion <rcorystine@gmail.com>
2021-12-06 13:41:42 -0500

    added show action to user games controller

--- bg-backend/app/controllers/api/user_games_controller.rb
+++ bg-backend/app/controllers/api/user_games_controller.rb
@@ -1,4 +1,11 @@
 class Api::UserGamesController < ApplicationController
-    
+    def index
+        @user_games = UserGame.all
+        render json: @user_games
+    end
 
+    def show
+        @user_game = UserGame.find(params[:id])
+        render json: @user_game
+    end
 end
