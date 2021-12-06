45aa0054a6a041145c8f4225918b3ebaa86b3fbb
GoldenPavilion <rcorystine@gmail.com>
2021-12-06 12:48:04 -0500

    added show action to comment controller

--- bg-backend/app/controllers/api/comments_controller.rb
+++ bg-backend/app/controllers/api/comments_controller.rb
@@ -1,5 +1,11 @@
 class Api::CommentsController < ApplicationController
-    
+    def index
+        @comments = Comment.all
+        render json: @comments
+    end
 
-
+    def show
+        @comment = Comment.find(params[:id])
+        render json: @comment
+    end
 end
