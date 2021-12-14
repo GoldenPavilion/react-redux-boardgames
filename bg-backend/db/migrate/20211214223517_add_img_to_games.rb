781c3527d2afe048d7154ab0a6d99a05b31e9195
GoldenPavilion <rcorystine@gmail.com>
2021-12-14 17:37:12 -0500

    added img column to games table and migrated

---
+++ bg-backend/db/migrate/20211214223517_add_img_to_games.rb
@@ -0,0 +1,5 @@
+class AddImgToGames < ActiveRecord::Migration[6.1]
+  def change
+    add_column :games, :img, :string
+  end
+end
--- bg-backend/db/schema.rb
+++ bg-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_183533) do
+ActiveRecord::Schema.define(version: 2021_12_14_223517) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_12_06_183533) do
     t.string "description_preview"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img"
   end
 
   create_table "user_games", force: :cascade do |t|
