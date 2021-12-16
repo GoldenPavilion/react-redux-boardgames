c293156bdbb737bdce281de1aef65f9502a55f33
GoldenPavilion <rcorystine@gmail.com>
2021-12-16 13:14:36 -0500

    imported combineReducers from redux

--- bg-frontend/src/index.js
+++ bg-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
