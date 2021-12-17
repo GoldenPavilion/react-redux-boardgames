ed9fc9642890d81603d6fbdf4f71fe8b4ba17fb4
GoldenPavilion <rcorystine@gmail.com>
2021-12-17 16:49:49 -0500

    changed from class to functional component

--- bg-frontend/src/components/Login.js
+++ bg-frontend/src/components/Login.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class Login extends Component {
-    state = {
-        username: '',
-        password: ''
-    }
-
-    handleOnChange = event => {
-        this.setState({[event.target.name]: event.target.value})
-    }
-    
-    render(){
-        return(
-            <div className="login">
-                <form className="login-form">
-                    <label>Username:</label>
-                    <input
-                        className="username" 
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleOnChange} 
-                    />
-                    <label>Password:</label>
-                    <input 
-                        className="password"
-                        type="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleOnChange}
-                    />
-                    <button type="submit" value="Login">Login</button>
-                </form>
-            </div>
-        )
-    }
+const Login = ({ username, password }) => {
+    return(
+        <div className="login">
+            <form className="login-form">
+                <label>Username:</label>
+                <input
+                    className="username" 
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleOnChange} 
+                />
+                <label>Password:</label>
+                <input 
+                    className="password"
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleOnChange}
+                />
+                <button type="submit" value="Login">Login</button>
+            </form>
+        </div>
+    )
 }
+
 
 const mapStateToProps = state => {
     return {
-        username: state.loginForm.username,
-        password: state.loginForm.password
+        username: state.loginReducer.username,
+        password: state.loginReducer.password
     }
 }
 
