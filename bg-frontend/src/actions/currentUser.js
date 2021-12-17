1aa30122ff4f32af2dae5417e48f3bef59deee07
GoldenPavilion <rcorystine@gmail.com>
2021-12-16 21:43:08 -0500

    created temporary login action in currentUser

--- bg-frontend/src/actions/currentUser.js
+++ bg-frontend/src/actions/currentUser.js
@@ -4,3 +4,11 @@ export const setCurrentUser = user => {
         user: user
     };
 };
+
+export const login = creds => {
+    return dispatch => {
+        return fetch("http://localhost:3000/sessions")
+        .then(resp => resp.json())
+        .then(user => console.log(user))
+    }
+}
