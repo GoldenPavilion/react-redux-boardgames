4694ffd3c7ec776c33d1222899942fc0206343e8
GoldenPavilion <rcorystine@gmail.com>
2021-12-21 14:34:06 -0500

    created getCurrentUser action

--- bg-frontend/src/actions/currentUser.js
+++ bg-frontend/src/actions/currentUser.js
@@ -27,3 +27,24 @@ export const login = creds => {
         .catch(console.log)
     }
 }
+
+export const getCurrentUser = creds => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/login", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(resp => resp.json())
+        .then(user => {
+            if (user.error){
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        }
+        )
+        .catch(console.log)
+    }
+}
