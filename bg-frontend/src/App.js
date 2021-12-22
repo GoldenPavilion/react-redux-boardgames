34bba2ba26a2e5354442df2e974d043dca434c17
GoldenPavilion <rcorystine@gmail.com>
2021-12-21 20:34:33 -0500

    calls getCurrentsUser if component mounts

--- bg-frontend/src/App.js
+++ bg-frontend/src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import React, { Component } from 'react';
 import Login from "./components/Login";
 import { connect } from 'react-redux';
-import { getCurrentUser } from "../actions/currentUser";
+import { getCurrentUser } from "./actions/currentUser";
 
 class App extends Component {
+
+  componentDidMount() {
+    getCurrentUser()
+  }
+
   render() {
     return (
       <Login />
