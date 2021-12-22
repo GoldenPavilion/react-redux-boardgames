6b8ee9b92f674ff775126a7e7a5ede8388664fc2
GoldenPavilion <rcorystine@gmail.com>
2021-12-21 20:29:51 -0500

    imported connect getCurrentUser

--- bg-frontend/src/App.js
+++ bg-frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, { Component } from 'react';
-import Login from "./components/Login"
+import Login from "./components/Login";
+import { connect } from 'react-redux';
+import { getCurrentUser } from "../actions/currentUser";
 
 class App extends Component {
   render() {
