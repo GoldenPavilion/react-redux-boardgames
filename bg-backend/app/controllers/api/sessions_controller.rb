1a851379d1a00eb013e5aaba0ccfadafa4039f97
GoldenPavilion <rcorystine@gmail.com>
2021-12-22 12:56:30 -0500

    simplified code

--- bg-backend/app/controllers/api/sessions_controller.rb
+++ bg-backend/app/controllers/api/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::SessionsController < ApplicationController
 
     def create
-        @user = User.find_by(username: params[:username])
-        if @user ##&& @user.authenticate(params[:password])
+        @user = User.find_by(username: params[:session][:username])
+        if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
             render json: @user
         else
--- bg-backend/app/controllers/application_controller.rb
+++ bg-backend/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     include ::ActionController::Cookies
 
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        User.find_by(id: session[:user_id])
     end
 
     def logged_in?
