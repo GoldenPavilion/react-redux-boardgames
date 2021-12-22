284a4a8ba5e378a2463e8bd6b2181d8298e25495
GoldenPavilion <rcorystine@gmail.com>
2021-12-22 18:08:54 -0500

    added delete route to remove sessions

--- bg-backend/config/routes.rb
+++ bg-backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post '/api/login', to: "api/sessions#create"
   get '/api/get_current_user', to: "api/sessions#get_current_user"
+  delete '/api/logout', to: "api/sessions#destroy"
 
   
   namespace :api do
