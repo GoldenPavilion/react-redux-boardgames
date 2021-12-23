e4c3d1ad098702ff1349310adc5076cd28f14933
GoldenPavilion <rcorystine@gmail.com>
2021-12-22 19:52:10 -0500

    stubbed out Logout function that render logout button

---
+++ bg-frontend/src/components/Logout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../actions/currentUser';
+
+
+const Logout = ({ logout }) => {
+    
+    return(
+        <div className="logout">
+            <form onSubmit={logout}>
+                <input type="submit" value="Logout" />
+            </form>
+        </div>
+    )
+}
+
+export default connect(null, { logout })(Logout);
