482b25b61b9c1366dbba606581bc2eadcf5ac4d5
GoldenPavilion <rcorystine@gmail.com>
2021-12-23 21:52:06 -0500

    stubbed out MainContainer

---
+++ bg-frontend/src/components/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContainer = () => {
+    return(
+        <div className="main-container">
+            <p>THE BODY OF THE APP</p>
+        </div>
+    )
+}
+
+export default MainContainer;
