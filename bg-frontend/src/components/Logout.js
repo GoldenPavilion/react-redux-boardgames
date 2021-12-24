667c0fb047967d6611fb237205bb8ac4df45fdde
GoldenPavilion <rcorystine@gmail.com>
2021-12-23 19:31:40 -0500

    stubbed out basic NavBar render component

--- bg-frontend/src/components/Logout.js
+++ bg-frontend/src/components/Logout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions/currentUser';
 
-
 const Logout = ({ logout }) => {
     
     const handleSubmit = (event) => {
