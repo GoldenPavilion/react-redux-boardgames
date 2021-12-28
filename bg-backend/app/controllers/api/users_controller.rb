438b0e781315fff958d75fc436099dcbd1623236
GoldenPavilion <rcorystine@gmail.com>
2021-12-27 19:54:07 -0500

    sets session when use creates a new account

--- bg-backend/app/controllers/api/users_controller.rb
+++ bg-backend/app/controllers/api/users_controller.rb
@@ -14,6 +14,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
