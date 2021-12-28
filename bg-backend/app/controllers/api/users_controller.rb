fbaee467e2625173a206dae146c7e4138146b15d
GoldenPavilion <rcorystine@gmail.com>
2021-12-27 19:52:01 -0500

    removed comments from controller

--- bg-backend/app/controllers/api/users_controller.rb
+++ bg-backend/app/controllers/api/users_controller.rb
@@ -1,18 +1,15 @@
 class Api::UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
-  #GET /users
   def index
     @users = User.all
     render json: UserSerializer.new(@users)
   end
 
-  # GET /users/1
   def show
     render json: UserSerializer.new(@user)
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
@@ -23,27 +20,11 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  #def update
-   # if @user.update(user_params)
-    #  render json: @user
-    #else
-     # render json: @user.errors, status: :unprocessable_entity
-    #end
-  #end
-
-  # DELETE /users/1
-  #def destroy
-    #@user.destroy
-  #end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :password_digest, :email, :location, :game_host)
     end
