a2d0650b184a4861e9ec1841bc597259e41dffe2
GoldenPavilion <rcorystine@gmail.com>
2021-12-28 19:09:27 -0500

    calls UserSerializer and passes in current_user to better structure data

--- bg-backend/app/controllers/api/sessions_controller.rb
+++ bg-backend/app/controllers/api/sessions_controller.rb
@@ -14,7 +14,7 @@ class Api::SessionsController < ApplicationController
 
     def get_current_user
         if logged_in?
-            render json: current_user
+            render json: UserSerializer.new(current_user)
         else
             render json: {
                 error: "No Login"
