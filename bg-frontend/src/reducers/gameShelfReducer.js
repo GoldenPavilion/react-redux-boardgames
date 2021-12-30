9a6f7a133f92a6af9e9fef1a7b5f4c7a78fb648f
GoldenPavilion <rcorystine@gmail.com>
2021-12-30 14:29:28 -0500

    created SET_GAME_SHELF case to be called in actions

--- bg-frontend/src/reducers/gameShelfReducer.js
+++ bg-frontend/src/reducers/gameShelfReducer.js
@@ -1,5 +1,7 @@
 const gameShelfReducer = (state = [], action) => {
     switch(action.type) {
+        case "SET_GAME_SHELF":
+            return action.games
         default:
             return state
     }
