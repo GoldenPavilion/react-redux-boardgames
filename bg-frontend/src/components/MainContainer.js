a9d1f467df73ca5db91e9f118f151c734fab94bc
GoldenPavilion <rcorystine@gmail.com>
2021-12-30 18:17:55 -0500

    imports stubbed out GameShelf and places component in container

--- bg-frontend/src/components/MainContainer.js
+++ bg-frontend/src/components/MainContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GameShelf from './GameShelf';
 
 const MainContainer = () => {
     return(
         <div className="main-container">
             <p>THE BODY OF THE APP</p>
+            <GameShelf />
         </div>
     )
 }
