4c7b3f3ec096638f985a0e8d78ccea4ac5d3134d
GoldenPavilion <rcorystine@gmail.com>
2021-12-29 19:51:07 -0500

    fleshed out getGameShelf by fetch gamesdata

--- bg-frontend/src/actions/gameShelf.js
+++ bg-frontend/src/actions/gameShelf.js
@@ -5,9 +5,23 @@ export const setGameShelf = games => {
     }
 }
 
-export const getGameShelf = games => {
-    return {
-        type: "GET_GAME_SHELF",
-        games
+export const getGameShelf = () => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/games", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+            .then(resp => resp.json())
+            .then(response => {
+                if (response.error) {
+                    alert(response.error)
+                } else {
+                    dispatch(setGameShelf())
+                }
+            })
+            .catch(console.log)
+        })
     }
 }
