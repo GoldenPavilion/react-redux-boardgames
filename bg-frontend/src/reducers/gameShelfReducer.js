3039f69e62d5b8c86b9bb4504760c7e6485da340
GoldenPavilion <rcorystine@gmail.com>
2021-12-30 17:16:27 -0500

    added RESET_GAME_SHELF case in reducer to clear data from store on logout

--- bg-frontend/src/reducers/gameShelfReducer.js
+++ bg-frontend/src/reducers/gameShelfReducer.js
@@ -2,6 +2,8 @@ const gameShelfReducer = (state = [], action) => {
     switch(action.type) {
         case "SET_GAME_SHELF":
             return action.games
+        case "RESET_GAME_SHELF":
+            return null
         default:
             return state
     }
