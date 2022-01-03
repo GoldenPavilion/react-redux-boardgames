3b2f32350111867dd04293a9799782a44eee4583
GoldenPavilion <rcorystine@gmail.com>
2022-01-03 18:02:33 -0500

    importing components for login and signup

--- bg-frontend/src/App.js
+++ bg-frontend/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import React, { Component } from 'react';
 import NavBar from './components/NavBar.js';
-import MainContainer from './components/MainContainer.js'
+import MainContainer from './components/MainContainer.js';
+import Login from './components/Login.js';
+import SignUp from './components/SignUp.js';
 import { connect } from 'react-redux';
 import { getCurrentUser } from "./actions/currentUser";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -18,7 +20,7 @@ class App extends Component {
         <NavBar />
           <Router>
             <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/signup' component={SignUp} />
           </Router>
         <MainContainer />
       </div>
