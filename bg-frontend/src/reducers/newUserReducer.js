536ab358d5fc452c2544a859abb440a35a2bbb7d
GoldenPavilion <rcorystine@gmail.com>
2022-01-03 19:44:14 -0500

    created reset signup form action in newUserReducer

--- bg-frontend/src/reducers/newUserReducer.js
+++ bg-frontend/src/reducers/newUserReducer.js
@@ -10,6 +10,8 @@ const newUserReducer = (state = initState, action) => {
     switch (action.type) {
         case "UPDATE_SIGNUP_FORM":
             return action.formData
+        case "RESET_SIGNUP_FORM":
+            return initState
         default:
             return state;
     }
