6bf3c7ef6026b5221942513532608d5cea4606b5
GoldenPavilion <rcorystine@gmail.com>
2022-01-04 18:20:16 -0500

    imported and renders UserProfile in Profile container

--- bg-frontend/src/components/ProfileContainer.js
+++ bg-frontend/src/components/ProfileContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import GameShelf from './GameShelf';
+import UserProfile from './UserProfile';
 
 const ProfileContainer = () => {
     return(
-        <GameShelf />
+        <div className="profile-container">
+            <UserProfile />
+            <GameShelf />
+        </div>
     )
 }
 
