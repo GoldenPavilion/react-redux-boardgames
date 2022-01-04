4acb98fbf6768522c4b675e2d6f14758ce5689ce
GoldenPavilion <rcorystine@gmail.com>
2022-01-04 12:52:59 -0500

    imported Home component and render it when no current user is present

--- bg-frontend/src/components/MainContainer.js
+++ bg-frontend/src/components/MainContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import GameShelf from './GameShelf';
+import Home from './Home'
 
 const MainContainer = ({ currentUser }) => {
     return(
         <div className="main-container">
-            { currentUser ? <GameShelf /> : <p>THE BODY OF THE APP</p>}
+            { currentUser ? <GameShelf /> : <Home />}
         </div>
     )
 }
