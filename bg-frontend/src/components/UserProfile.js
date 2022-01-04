6a517649c62e8ca36501ba165e541ed7d1882762
GoldenPavilion <rcorystine@gmail.com>
2022-01-04 18:52:00 -0500

    removed mapStateToProps and connect

--- bg-frontend/src/components/UserProfile.js
+++ bg-frontend/src/components/UserProfile.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
+
 
 const UserProfile = props => {
+    
     return(
         <div></div>
     )
 }
 
-const mapStateToProps = state => {
-    return {
-        user: state.currentUserReducer
-    }
-}
-
-export default connect(mapStateToProps)(UserProfile);
+export default UserProfile;
