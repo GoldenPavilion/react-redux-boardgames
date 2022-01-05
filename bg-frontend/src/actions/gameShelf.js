d91217b4c15e90faf137f70ca371eedeb92e5d1a
GoldenPavilion <rcorystine@gmail.com>
2022-01-04 22:40:02 -0500

    added getAllGames action

--- bg-frontend/src/actions/gameShelf.js
+++ bg-frontend/src/actions/gameShelf.js
@@ -31,3 +31,24 @@ export const getGameShelf = () => {
             .catch(console.log)
     }
 }
+
+export const getAllGames = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/games", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => resp.json())
+            .then(response => {
+                if (response.error) {
+                    alert(response.error)
+                } else {
+                    dispatch(setGameShelf(response.data))
+                }
+            })
+            .catch(console.log)
+    }
+}
