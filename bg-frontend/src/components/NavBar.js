33782143b3fdefd9b7ad54534bdc254980bb69c0
GoldenPavilion <rcorystine@gmail.com>
2022-01-04 20:19:37 -0500

    set up basic links in navbar

--- bg-frontend/src/components/NavBar.js
+++ bg-frontend/src/components/NavBar.js
@@ -7,6 +7,9 @@ import { Link } from 'react-router-dom';
 const NavBar = ({ currentUser }) => {
     return(
         <div className="nav-bar">
+            <Link to="/">Home </Link>
+            <Link to="/login">Login </Link>
+            <Link to="/signup">Sign Up</Link>
             { currentUser ? <Logout /> : ""}
         </div>
     )
