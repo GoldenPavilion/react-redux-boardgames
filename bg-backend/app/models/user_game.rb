12df1dd97b48d4395d1cb6bcf3104abe9d974793
GoldenPavilion <rcorystine@gmail.com>
2022-01-07 14:19:16 -0500

    added validations to make sure the user doesn't have multiple of the same game

--- bg-backend/app/models/user_game.rb
+++ bg-backend/app/models/user_game.rb
@@ -3,4 +3,6 @@ class UserGame < ApplicationRecord
     belongs_to :game
 
     validates :user_id, :game_id, presence: true
+    validates :user_id, uniqueness: { scope: :game_id, 
+        message: "can only own a game once" }
 end
