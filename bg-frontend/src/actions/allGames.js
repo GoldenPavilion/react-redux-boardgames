8468b3cf7b845a89c16a3422c08cc52e62bc2ded
GoldenPavilion <rcorystine@gmail.com>
2022-01-11 18:34:22 -0500

    added updateSearchForm action to allGames

--- bg-frontend/src/actions/allGames.js
+++ bg-frontend/src/actions/allGames.js
@@ -25,3 +25,10 @@ export const getAllGames = () => {
         .catch(console.log)
     }
 }
+
+export const updateSearchForm = searchData => {
+    return {
+        type: "UPDATE_SEARCH_FORM",
+        searchData
+    }
+}
