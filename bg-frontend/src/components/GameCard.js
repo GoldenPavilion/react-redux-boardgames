def79efce4de4de99bc26e75d697a1618adfecf9
GoldenPavilion <rcorystine@gmail.com>
2022-01-11 15:20:05 -0500

    restored to functional component structure

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import AddGameButton from './AddGameButton'
 
-class GameCard extends Component {    
+const GameCard = props => {    
 
-    render(){
-        const game = this.props.game.attributes 
-        const ids = { game_id: parseInt(this.props.game.id), user_id: parseInt(this.props.currentUser.id) }
+    const game = props.game.attributes 
+    const ids = { game_id: parseInt(props.game.id), user_id: parseInt(props.currentUser.id) }
 
-        return (
-            <div className="card" style={{width: 320}}>
-                <img className="card-img-top" src={ game.img } alt="Game Title"></img>
-                <div className="card-body">
-                    <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
-                    <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
-                    <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
-                    <p className="card-text">Description: { game.description_preview }</p>
-                    <AddGameButton currentUser={this.props.currentUser} game={game} ids={ids} />
-                </div>
+    return (
+        <div className="card" style={{width: 320}}>
+            <img className="card-img-top" src={ game.img } alt="Game Title"></img>
+            <div className="card-body">
+                <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
+                <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
+                <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
+                <p className="card-text">Description: { game.description_preview }</p>
+                <AddGameButton currentUser={props.currentUser} game={game} ids={ids} />
             </div>
-    )}
+        </div>
+    )
 }
 
 
