fcd81f92b727419f943f1c61dc7b8157d95cd88a
GoldenPavilion <rcorystine@gmail.com>
2022-01-11 15:46:45 -0500

    added basic search form JSX

--- bg-frontend/src/components/SearchForm.js
+++ bg-frontend/src/components/SearchForm.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const SearchForm = props => {
-
+    return (
+        <div className="search">
+            <form>
+                <div class="form-group">
+                    <input type="text" className="form-control" placeholder="Search..." />
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default SearchForm;
