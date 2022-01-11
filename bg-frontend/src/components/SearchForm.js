b2acab50a0139ff64881d558010e39545a915d7b
GoldenPavilion <rcorystine@gmail.com>
2022-01-11 18:42:27 -0500

    created basic handleOnChange method

--- bg-frontend/src/components/SearchForm.js
+++ bg-frontend/src/components/SearchForm.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { updateSearchForm } from '../actions/allGames';
+import searchReducer from '../reducers/searchReducer';
 
 const SearchForm = props => {
+
+    const handleOnChange = event => {
+        const { name, value } = event.target
+        const updatedData = {
+            ...searchReducer,
+            [name]: value
+        }
+        updateSearchForm(updatedData)
+    }
+
     return (
         <div className="search">
             <form>
                 <div class="form-group">
-                    <input type="text" className="form-control" placeholder="Search..." />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Search..." 
+                    />
                 </div>
             </form>
         </div>
