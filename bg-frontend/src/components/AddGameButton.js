952497fa662cb009ca0c1a49ba986d615f172e7d
GoldenPavilion <rcorystine@gmail.com>
2022-01-11 13:27:36 -0500

    changed AddGameButton from functional to state-based

--- bg-frontend/src/components/AddGameButton.js
+++ bg-frontend/src/components/AddGameButton.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddGameButton = props => {
+class AddGameButton extends Component {
+    state = {
+        buttonText: "Add To Shelf",
+        disabled: false
+    }
 
-    const userGames = props.currentUser.attributes.games
-    const currentGame = props.game
+    render(){
+        const userGames = this.props.currentUser.attributes.games
+        const currentGame = this.props.game
     
-    if(userGames.some(game => game.name === currentGame.name)){
-        return(<button className="btn btn-secondary" disabled="true">Shelved!</button>)
-    } else {
-        return(<button className="btn btn-secondary" onClick={props.handleClick}>Add To Shelf</button>)
+        if(userGames.some(game => game.name === currentGame.name)){
+            return(<button className="btn btn-secondary" disabled="true">Shelved!</button>)
+        } else {
+            return(<button className="btn btn-secondary" onClick={this.props.handleClick}>Add To Shelf</button>)
+        }
     }
 }
 
