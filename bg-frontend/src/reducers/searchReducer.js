5f82b4b0c7c893d644324992c5bbec97f13fe74c
GoldenPavilion <rcorystine@gmail.com>
2022-01-13 14:00:22 -0500

    created reset login form type

--- bg-frontend/src/reducers/searchReducer.js
+++ bg-frontend/src/reducers/searchReducer.js
@@ -6,6 +6,8 @@ const searchReducer = (state = initState, action) => {
     switch(action.type){
         case "UPDATE_SEARCH_FORM":
             return action.searchData
+        case "RESET_SEARCH_FORM":
+            return initState;
         default:
             return state;
     }
