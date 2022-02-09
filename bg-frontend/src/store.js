d7243947742f1cd0dfa4183cbaf648d0be8b5b37
GoldenPavilion <rcorystine@gmail.com>
2022-02-09 18:39:50 -0500

    imported reducers to combine

--- bg-frontend/src/store.js
+++ bg-frontend/src/store.js
@@ -1,1 +1,10 @@
 import { createStore, applyMiddleware, compose, combineReducer } from 'redux';
+
+import usersReducer from './reducers/usersReducer';
+import currentUserReducer from './reducers/currentUserReducer';
+import loginReducer from './reducers/loginReducer';
+import newUserReducer from './reducers/newUserReducer';
+import gameShelfReducer from './reducers/gameShelfReducer';
+import allGamesReducer from './reducers/allGamesReducer';
+import searchReducer from './reducers/searchReducer';
+
