3ab54b1c11fc462556505a135f23e62748ea975e
GoldenPavilion <rcorystine@gmail.com>
2022-02-12 17:47:24 -0500

    Imported Link from react-router-dom

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import AddGameButton from './AddGameButton';
 
 const GameCard = props => {    
