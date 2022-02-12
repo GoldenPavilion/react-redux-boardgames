c034c15c879483df5927997e8139c0823a6b876b
GoldenPavilion <rcorystine@gmail.com>
2022-02-11 21:54:12 -0500

    set up basic link in game card to test nested route

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -12,6 +12,7 @@ const GameCard = props => {
             <img className="card-img-top" src={ game.img } alt="Game Title"></img>
             <div className="card-body">
                 <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
+                <Link to="/games/:gameId" game={ game }/>
                 <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
                 <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
                 <p className="card-text">Description: { game.description_preview }</p>
