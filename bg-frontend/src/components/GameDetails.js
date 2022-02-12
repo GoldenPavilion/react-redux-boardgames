ecb879d379345bcdb862b4cf69c1ea7feb2311ed
GoldenPavilion <rcorystine@gmail.com>
2022-02-11 22:09:15 -0500

    set up JSX render for GameDetails

--- bg-frontend/src/components/GameDetails.js
+++ bg-frontend/src/components/GameDetails.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const GameDetails = { game } => {
+const GameDetails = ({ game }) => {
 
     return (
         <div className="game-page">
             <img className="game-img" src={ game } alt="Game Title"></img>
+            <div className="details">
+                <h2 className="title">{game.name}</h2>
+                <p className="text">Release Year: {game.year_published}</p>
+                <p className="text">Players: {game.min_players} - {game.max_players}</p>
+                <p className="text">Play Time: {game.min_playtime} - {game.max_playtime}</p>
+                <p className="text">Description: {game.description_preview}</p>
+            </div>
         </div>
     )
 }
+
+export default GameDetails;
