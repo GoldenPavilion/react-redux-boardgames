6fdcb08c0d76ba098a2c2b8c0e661b956655c61d
GoldenPavilion <rcorystine@gmail.com>
2022-02-14 14:54:31 -0500

    interpolated game id into Link

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -13,7 +13,7 @@ const GameCard = props => {
             <img className="card-img-top" src={ game.img } alt="Game Title"></img>
             <div className="card-body">
                 <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
-                <Link to="/games/:gameId" game={ game }>Details</Link>
+                <Link to={`${game.id}`} game={ game }>Details</Link>
                 <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
                 <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
                 <p className="card-text">Description: { game.description_preview }</p>
