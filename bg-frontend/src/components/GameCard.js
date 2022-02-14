84847ee2e623802300cefd608935e862523bac57
GoldenPavilion <rcorystine@gmail.com>
2022-02-14 16:15:46 -0500

    added key to link

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -13,7 +13,7 @@ const GameCard = props => {
             <img className="card-img-top" src={ game.img } alt="Game Title"></img>
             <div className="card-body">
                 <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
-                <Link to={`${props.game.id}`} game={ game }>Details</Link>
+                <Link key={props.game.id} to={`${props.game.id}`} game={ game }>Details</Link>
                 <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
                 <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
                 <p className="card-text">Description: { game.description_preview }</p>
