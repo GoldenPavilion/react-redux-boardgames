438c9a2382c3e9189c6f73908f6f5cfd68c9d1e9
GoldenPavilion <rcorystine@gmail.com>
2022-02-14 15:50:27 -0500

    passed in game id as props

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -7,13 +7,13 @@ const GameCard = props => {
 
     const game = props.game.attributes 
     const ids = { game_id: parseInt(props.game.id), user_id: parseInt(props.currentUser.id) }
-
+    
     return (
         <div className="card" style={{width: 320}}>
             <img className="card-img-top" src={ game.img } alt="Game Title"></img>
             <div className="card-body">
                 <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
-                <Link to={`${game.id}`} game={ game }>Details</Link>
+                <Link to={`${props.game.id}`} game={ game }>Details</Link>
                 <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
                 <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
                 <p className="card-text">Description: { game.description_preview }</p>
