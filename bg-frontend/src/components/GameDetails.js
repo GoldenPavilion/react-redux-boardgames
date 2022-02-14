a03c1458039ec766a9bcf2b2b43fbc0b70af1bae
GoldenPavilion <rcorystine@gmail.com>
2022-02-14 17:43:18 -0500

    passed in props instead of destructuring to test data

--- bg-frontend/src/components/GameDetails.js
+++ bg-frontend/src/components/GameDetails.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const GameDetails = ({ game }) => {
-    
+const GameDetails = props => {
+    debugger
     return (
         <div className="game-page">
-            <img className="game-img" src={ game } alt="Game Title"></img>
+            <img className="game-img" src={ props.game.img } alt="Game Title"></img>
             <div className="details">
-                <h2 className="title">{game.name}</h2>
-                <p className="text">Release Year: {game.year_published}</p>
-                <p className="text">Players: {game.min_players} - {game.max_players}</p>
-                <p className="text">Play Time: {game.min_playtime} - {game.max_playtime}</p>
-                <p className="text">Description: {game.description_preview}</p>
+                <h2 className="title">{props.game.name}</h2>
+                <p className="text">Release Year: {props.game.year_published}</p>
+                <p className="text">Players: {props.game.min_players} - {props.game.max_players}</p>
+                <p className="text">Play Time: {props.game.min_playtime} - {props.game.max_playtime}</p>
+                <p className="text">Description: {props.game.description_preview}</p>
             </div>
         </div>
     )
