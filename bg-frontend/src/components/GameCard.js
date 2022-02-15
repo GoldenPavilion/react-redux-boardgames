7c02d2436964d3f3b376919a3f04a6eab0093407
GoldenPavilion <rcorystine@gmail.com>
2022-02-14 20:43:08 -0500

    removed game prop since Link cannot take props in v6

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -8,12 +8,13 @@ const GameCard = props => {
     const game = props.game.attributes 
     const ids = { game_id: parseInt(props.game.id), user_id: parseInt(props.currentUser.id) }
 
+    debugger
     return (
         <div className="card" style={{width: 320}}>
             <img className="card-img-top" src={ game.img } alt="Game Title"></img>
             <div className="card-body">
                 <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
-                <Link key={props.game.id} to={`${props.game.id}`} game={ props.game }>Details</Link>
+                <Link key={props.game.id} to={`${props.game.id}`} >Details</Link>
                 <p className="card-text">Players: { game.min_players } - { game.max_players }</p>
                 <p className="card-text">Play Time: { game.min_playtime } - { game.max_playtime }</p>
                 <p className="card-text">Description: { game.description_preview }</p>
