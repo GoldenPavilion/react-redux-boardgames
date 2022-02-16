c8e58e76f7d04727754c622d70d3a2b272e53d4f
GoldenPavilion <rcorystine@gmail.com>
2022-02-15 21:29:31 -0500

    details appear at very bottom of search results

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -8,7 +8,6 @@ const GameCard = props => {
     const game = props.game.attributes 
     const ids = { game_id: parseInt(props.game.id), user_id: parseInt(props.currentUser.id) }
 
-   
     return (
         <div className="card" style={{width: 320}}>
             <img className="card-img-top" src={ game.img } alt="Game Title"></img>
