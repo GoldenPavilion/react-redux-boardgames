a43ea6ee54901c3af84a0d45f9a5d5610a7f7fbc
GoldenPavilion <rcorystine@gmail.com>
2022-02-22 17:27:57 -0500

    imported useNavigate into GameCard

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AddGameButton from './AddGameButton';
 
 const GameCard = props => {    
