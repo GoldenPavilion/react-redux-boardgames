80f90c0a84ac78ebbab8204217fe598ee0ab86bd
GoldenPavilion <rcorystine@gmail.com>
2022-02-22 18:11:57 -0500

    removed useNavigate tools as tests failed

--- bg-frontend/src/components/GameCard.js
+++ bg-frontend/src/components/GameCard.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AddGameButton from './AddGameButton';
 
 const GameCard = props => {    
 
-    const game = props.game.attributes;
-    const navigate = useNavigate(); 
-    const ids = { game_id: parseInt(props.game.id), user_id: parseInt(props.currentUser.id) };
-
-    const handleOnClick = () => {
-        navigate(`/games/${props.games.id}`)
-    }
+    const game = props.game.attributes 
+    const ids = { game_id: parseInt(props.game.id), user_id: parseInt(props.currentUser.id) }
 
     return (
         <div className="card" style={{width: 320}}>
-            {/*<Link key={props.game.id} to={`${props.game.id}`} state={{ game: props.game}}>*/}
-                <img className="card-img-top" width="280" height="280" src={ game.img } onClick={handleOnClick} alt="Game Title" />
-            {/* </Link> */}
+            <Link key={props.game.id} to={`${props.game.id}`} state={{ game: props.game}}>
+                <img className="card-img-top" width="280" height="280" src={ game.img } alt="Game Title" />
+            </Link>
             <div className="card-body">
                 <h3 className="card-title">{ game.name } ({ game.year_published})</h3>
                 <br />
