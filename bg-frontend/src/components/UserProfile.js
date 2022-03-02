99f78aad88c70dc2b319834071828e9051fd604a
GoldenPavilion <rcorystine@gmail.com>
2022-03-02 16:47:10 -0500

    updated Profile styling

--- bg-frontend/src/components/UserProfile.js
+++ bg-frontend/src/components/UserProfile.js
@@ -6,7 +6,7 @@ const UserProfile = props => {
     const user = props.user.attributes
     
     return(
-        <div className="card" id="user-card"style={{width: 300}}>
+        <div className="card" id="user-card">
             <div className="card-body">
                 { user.game_host ? <h3 className="card-title">{ user.username }(Host)</h3> : <h3>{ user.username }</h3>}        
                 <p className="card-text">Location: { user.location} </p>
