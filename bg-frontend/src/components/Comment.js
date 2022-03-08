0df64cca6a9c53665078e6c924b9ec6d260ed938
GoldenPavilion <rcorystine@gmail.com>
2022-03-08 18:39:24 -0500

    displays comment content

--- bg-frontend/src/components/Comment.js
+++ bg-frontend/src/components/Comment.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Comment = props => {
-    
     return(
-        <div className="comments">
-            <h3>Comments:</h3>
-            
+        <div className="comment">
+            <p>{ props.comment.content }</p>
         </div>
     )
 }
