4d5cc27ec78d3dadd5ae7d0722308583d5109cf1
GoldenPavilion <rcorystine@gmail.com>
2022-03-08 18:06:48 -0500

    stubbed out Comments component

---
+++ bg-frontend/src/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = () => {
+    return(
+        <div className="comments">
+            <h3>Comments:</h3>
+        </div>
+    )
+}
+
+export default Comments;
