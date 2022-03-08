46f3f274f58ab2ace1d5b843cb973971a14f6d5d
GoldenPavilion <rcorystine@gmail.com>
2022-03-08 18:45:16 -0500

    renders ContentForm in Comments

--- bg-frontend/src/components/Comments.js
+++ bg-frontend/src/components/Comments.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Comment from './Comment.js';
+import CommentForm from './CommentForm.js';
 
 const Comments = props => {
     
     return(
         <div className="comments">
             <h3>Comments:</h3>
+            <CommentForm />
             { props.game.comments.map(comment => 
                 <Comment key={comment.id} comment={comment} />
             )}
