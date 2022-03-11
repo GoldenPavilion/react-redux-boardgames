63b6f436abbf67ed03e75dbd688f96b6b7ad0513
GoldenPavilion <rcorystine@gmail.com>
2022-03-10 20:58:26 -0500

    added reset comment form case

--- bg-frontend/src/reducers/commentReducer.js
+++ bg-frontend/src/reducers/commentReducer.js
@@ -6,6 +6,8 @@ const commentReducer = (state = initState, action) => {
     switch (action.type) {
         case "UPDATE_COMMENT_FORM":
             return action.formData
+        case "RESET_COMMENT_FORM":
+            return initState;
         default:
             return state;
     } 
