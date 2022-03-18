dead288c77f1f173c1e00ff55518d8efe29be842
GoldenPavilion <rcorystine@gmail.com>
2022-03-18 19:58:25 -0400

    updated initState to include game_id and user_id

--- bg-frontend/src/reducers/commentReducer.js
+++ bg-frontend/src/reducers/commentReducer.js
@@ -1,10 +1,13 @@
 const initState = {
     content: "",
+    game_id: null, 
+    user_id: null
 }
 
 const commentReducer = (state = initState, action) => {
     switch (action.type) {
         case "UPDATE_COMMENT_FORM":
+            debugger
             return action.formData
         case "RESET_COMMENT_FORM":
             return initState;
