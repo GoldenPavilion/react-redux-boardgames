50ab6064b5877613a6a92532140ddd95561868a2
GoldenPavilion <rcorystine@gmail.com>
2022-03-23 18:49:50 -0400

    adjusted content call

--- bg-frontend/src/components/Comment.js
+++ bg-frontend/src/components/Comment.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Comment = props => {
+    debugger
     return(
         <div className="comment">
-            <p>{ props.comment.content }</p>
+            <p>{ props.comment.attributes.content }</p>
         </div>
     )
 }
