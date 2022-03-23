57e396e909d7369c393c3dc40624d51326b417a5
GoldenPavilion <rcorystine@gmail.com>
2022-03-23 19:01:08 -0400

    added additional styling

--- bg-frontend/src/components/Comment.js
+++ bg-frontend/src/components/Comment.js
@@ -5,7 +5,7 @@ const Comment = props => {
     return(
         <div className="comment">
             <p>{ props.comment.attributes.content }</p>
-            <p className="comment-user"><i>{ props.comment.attributes.user.username}</i></p>
+            <p className="comment-user"><i>---{ props.comment.attributes.user.username}</i></p>
         </div>
     )
 }
