c16769182121619ace262cd073d49c6c2b68ac34
GoldenPavilion <rcorystine@gmail.com>
2022-03-22 21:48:30 -0400

    created getComents fetch

--- bg-frontend/src/actions/comments.js
+++ bg-frontend/src/actions/comments.js
@@ -10,3 +10,24 @@ export const resetComments = () => {
         type: 'RESET_COMMENTS'
     }
 }
+
+export const getComments = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/comments", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => resp.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            } else {
+                dispatch(setComments(response.data))
+            }
+        })
+        .catch(console.log)
+    }
+}
