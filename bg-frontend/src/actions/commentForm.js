06cfe533c0f66d2cbf49a8c2f50884191bbd56b5
GoldenPavilion <rcorystine@gmail.com>
2022-03-25 19:06:20 -0400

    deleted original addComment

--- bg-frontend/src/actions/commentForm.js
+++ bg-frontend/src/actions/commentForm.js
@@ -10,20 +10,3 @@ export const resetCommentForm = () => {
         type: 'RESET_COMMENT_FORM'
     }
 }
-
-export const addComment = (ids) => {
-    return dispatch => {
-        return fetch("http://localhost:3001/api/comments", {
-            credentials: "include",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(ids)
-        })
-        .then(resp => resp.json())
-        .then (response => {
-            dispatch(resetCommentForm())
-        })
-    }
-}
